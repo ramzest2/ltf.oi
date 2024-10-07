86fdcee78d7668ed2914c02cfea53ae159936378
ramzest2 <ayangkucantik69@gmail.com>
2024-10-07 23:00:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    SSE_ENDPOINT: 'http://localhost:8000/stream',
-    MESSAGE_ENDPOINT: 'http://localhost:8000/send-message',
-    AUDIO_ENDPOINT: 'http://localhost:8000/send-message',
+    SSE_ENDPOINT: "http://localhost:8000/stream",
+    MESSAGE_ENDPOINT: "http://localhost:8000/send-message",
+    AUDIO_ENDPOINT: "http://localhost:8000/send-message",
     // Настройки приложения
     APP_SETTINGS: {
         DEFAULT_LANGUAGE: 'ru',
@@ -9,12 +9,12 @@ const CONFIG = {
     },
     // Константы для пунктов меню
     MENU_ITEMS: {
-        SHAWARMA: 'shawarma',
-        PITA: 'pita',
-        HUMMUS: 'hummus',
-        CHICKEN_SHISH: 'chicken_shish',
-        GOZLEME: 'gozleme',
-        LENTIL_SOUP: 'lentil_soup'
+        SHAWARMA: "shawarma",
+        PITA: "pita",
+        HUMMUS: "hummus",
+        CHICKEN_SHISH: "chicken_shish",
+        GOZLEME: "gozleme",
+        LENTIL_SOUP: "lentil_soup"
     }
     // ... другие настройки ...
 };
