924570d853a9e7586c4852b9bf73e6eb711f72da
ramzest2 <ayangkucantik69@gmail.com>
2024-10-18 21:30:11 +0800

    Update app40.js

--- app40.js
+++ app40.js
@@ -33,9 +33,11 @@ function initAudioContext() {
 function connectWebSocket() {
     const serverIP = '192.168.1.8'; // Замените на реальный локальный IP-адрес вашего сервера
     const serverPort = 3000; // Убедитесь, что это соответствует порту вашего сервера
-    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    // const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const protocol = window.location.protocol === 'https:' ? 'ws:' : 'ws:';
     
     socket = new WebSocket(`${protocol}//${serverIP}:${serverPort}`);
+    
     socket.binaryType = 'arraybuffer';
 
     socket.onopen = () => {
