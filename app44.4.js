fb30210cee53b2d61949b4a50656b5082142f7b0
ramzest2 <ayangkucantik69@gmail.com>
2025-09-21 23:43:58 +0800

    Update app44.4.js

--- app44.4.js
+++ app44.4.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = '175874289746-tblhoqj20e7oa7qktjus6rqcqsi6h2df.apps.googleusercontent.com'; // замените своим client_id из Google Cloud Console
-const SCRIPT_ID = '1FeIuRn9y0NuUhO0rTp4QE0sedfDv6LKse87ItFOPl8-LnjPnFA1yS-w2'; // замените ID вашего Google Apps Script проекта
+const SCRIPT_ID = '1FeIuRn9y0NuUhO0rTp4QE0sedfDv6LKse87ItFOPl8-LnjPnFA1yS-w2'; // замените ID вашего Google Apps Script 
 
 const fillingPrices = {
   chicken: 25000,
